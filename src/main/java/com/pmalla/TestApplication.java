2d8d638d36e09f11466084633ad886fcdeccbfe5
Praveen Malla <mjspka@gmail.com>
2020-07-02 11:06:39 -0700

    first commit

---
+++ src/main/java/com/pmalla/TestApplication.java
@@ -0,0 +1,17 @@
+package com.pmalla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author pmalla
+ *
+ */
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+}
